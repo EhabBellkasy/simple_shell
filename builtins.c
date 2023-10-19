a49cccdfa22a450e42f715a43fc7ac0c9a3b2e19
EhabBellkasy <EhabBellkasy@gmail.com>
2023-10-18 19:41:29 -0700

    {New commit drop here}

---
+++ builtins.c
@@ -0,0 +1,138 @@
+#include "shell.h"
+
+/**
+ * exit_shell - exits the shell
+ * @args: arguments passed to exit
+ * @argv: argv passed to main function
+ * @count: number of commands executed
+ * @exit_status: exit status of the shell
+ * @alias_list: Array of stored aliases
+ * Return: void
+ */
+
+void exit_shell(char **args, char **argv, int count,
+int exit_status, alias_t **alias_list)
+{
+int status = exit_status;
+
+if (args[1])
+{
+if (str_numeric(args[1]) && atoi(args[1]) >= 0)
+status = atoi(args[1]);
+else
+{
+fprintf(stderr, "%s: %d: exit: Illegal number: %s\n",
+argv[0], count, args[1]);
+status = 2;
+}
+}
+free2darray(args);
+free_alias(alias_list);
+exit(status);
+}
+
+/**
+ * print_env - prints the current environment
+ *
+ * Return: void
+ */
+
+void print_env(void)
+{
+int i;
+
+for (i = 0; environ[i]; i++)
+{
+write(1, environ[i], strlen(environ[i]));
+write(1, "\n", 1);
+}
+}
+
+/**
+ * set_env - sets an environment variable
+ * @args: arguments passed to setenv
+ * @exit_status: exit status of the shell
+ *
+ * Return: void
+*/
+void set_env(char **args, int *exit_status)
+{
+int len = arr_len(args);
+
+if (len == 3)
+{
+if (setenv(args[1], args[2], 1) == -1)
+{
+perror("setenv");
+*exit_status = 2;
+}
+}
+}
+
+/**
+ * unset_env - unsets an environment variable
+ * @args: arguments passed to setenv
+ * @exit_status: exit status of the shell
+ *
+ * Return: void
+ */
+void unset_env(char **args, int *exit_status)
+{
+int len = arr_len(args);
+
+if (len == 2 && unsetenv(args[1]) == -1)
+{
+perror("unsetenv");
+*exit_status = 2;
+}
+}
+
+/**
+ * change_directory - changes directory
+ * @args: arguments passed
+ * @av: command line arguments passed to main
+ * @count: number of shell executions
+ * Return: 0 on success, relevant error on failure
+ */
+
+int change_directory(char **args, char **av, int count)
+{
+char *home_dir, *prev_dir, *current_dir, *new_dir;
+
+current_dir = getcwd(NULL, 0);
+if (args[1])
+{
+if (strcmp(args[1], "-") == 0)
+{
+prev_dir = getenv("OLDPWD");
+if (!prev_dir)
+prev_dir = current_dir;
+printf("%s\n", prev_dir);
+chdir(prev_dir);
+}
+else if (chdir(args[1]) == -1)
+{
+fprintf(stderr, "%s: %d: %s: can't cd to %s\n",
+av[0], count, args[0], args[1]);
+free(current_dir);
+return (1);
+}
+}
+else
+{
+home_dir = getenv("HOME");
+if (!home_dir)
+{
+free(current_dir);
+return (1);
+}
+chdir(home_dir);
+}
+new_dir = getcwd(NULL, 0);
+setenv("PWD", new_dir, 1);
+setenv("OLDPWD", current_dir, 1);
+free(current_dir);
+free(new_dir);
+return (0);
+}
+
---
+++ execute.c
@@ -0,0 +1,94 @@
+#include "shell.h"
+
+/**
+ * execute - executes command in child process
+ * @cmd: complete command line
+ * @av: command line arguments
+ * @count: keeps track of how many times shell was executed
+ * Return: exit status
+ */
+
+int execute(char **cmd, char **av, int count)
+{
+pid_t pid;
+int status;
+char *full_path;
+
+full_path = check_path(cmd[0]);
+if (!full_path)
+{
+fprintf(stderr, "%s: %d: %s: not found\n", av[0], count, cmd[0]);
+return (127);
+}
+
+pid = fork();
+
+if (pid == 0)
+{
+if (execve(full_path, cmd, environ) == -1)
+{
+free(full_path);
+return (127);
+}
+free(full_path);
+return (0);
+}
+else if (pid > 0)
+{
+wait(&status);
+if (WIFEXITED(status))
+{
+free(full_path);
+return (WEXITSTATUS(status));
+}
+}
+else
+{
+perror("fork");
+free(full_path);
+return (1);
+}
+free(full_path);
+return (WEXITSTATUS(status));
+}
+
+/**
+ * execute_file - Executes commands in file line by line
+ * @filename: name of file to execute
+ * @av: command line arguments passed to program
+ * @exit_status: exit status of our program
+ *
+ * Return: void
+ */
+
+void execute_file(char *filename, char **av, int *exit_status)
+{
+int fd, i, count = 0;
+char **lines, **command, buffer[1024];
+ssize_t n_read;
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+fprintf(stderr, "%s: %d: Can't open %s\n", av[0], count, av[1]);
+*exit_status = 127;
+return;
+}
+n_read = read(fd, buffer, sizeof(buffer));
+if (n_read <= 0)
+{
+close(fd);
+*exit_status = 0;
+return;
+}
+buffer[n_read] = '\0';
+lines = parse_file(buffer, "\n");
+for (i = 0; lines[i]; i++)
+{
+command = parse_file(lines[i], " \t\n");
+*exit_status = execute(command, av, count);
+free2darray(command);
+}
+free2darray(lines);
+close(fd);
+}
